b089e7422ad2d5443e9d580f644581de65c45604
Sérgio <sergioR.opah@dotzmkt.com.br>
2021-08-09 13:55:33 -0300

    atualizações

---
+++ DesafioSoftplan.Domain/Services/Interfaces/IJurosService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioSoftplan.Services.Services.Interfaces
+{
+    public interface IJurosService
+    {
+        Task<double> TaxaJuros();
+        Task<string> Calculajuros(double valorInicial, int meses);
+    }
+}
---
+++ DesafioSoftplan.Domain/Utils/UtilsHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesafioSoftplan.Services.Utils
+{
+    public class UtilsHelper
+    {
+    }
+}
